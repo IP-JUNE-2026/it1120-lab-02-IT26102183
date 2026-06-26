f3eda0ecba01a8ba569ce2827fc98b72f89e1e6c
IT26102183 <it26102183@my.sliit.lk>
2026-06-26 10:24:27 +0530

    add q3 answer

---
+++ Q3/IT26102183Lab2Q3.java
@@ -0,0 +1,10 @@
+public class IT26102183Lab2Q3 {
+	 
+	  public static void main ( String [] args)
+	  {
+		  double SideA = 3;
+		  double SideB = 4;
+		  double hypotenus = Math.sqrt (SideA * SideA + SideB * SideB);
+		  System.out.println ("Length of the hypotenus="+hypotenus);
+	  }
+}
