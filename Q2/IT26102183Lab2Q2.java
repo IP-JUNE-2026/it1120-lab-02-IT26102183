0da255dc227222b49b519a1a344f5a56bd3973dd
IT26102183 <it26102183@my.sliit.lk>
2026-06-26 10:23:47 +0530

    add q2 answer

---
+++ Q2/IT26102183Lab2Q2.java
@@ -0,0 +1,15 @@
+public class IT26102183Lab2Q2 {
+             
+			 public static void main (String [] args)
+		{
+		    double length = 10;
+			double squareperimeter = 4*length;
+			double PI = 22.0/7;
+			double radius = squareperimeter/(2*PI);
+
+			
+		System.out.println("Radius of the circle:"+radius);
+		}
+}
+			
+			
